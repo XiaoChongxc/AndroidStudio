ddcb34f19bc4469ee70ea148cbec8033b3282ce7
xiaochong_XC <871683587@qq.com>
2016-03-03 10:51:59 +0800

    11

---
+++ myapp/src/main/java/com/example/xc/myapp/activity/map/LocationActivity.java
@@ -0,0 +1,23 @@
+package com.example.xc.myapp.activity.map;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.xc.myapp.R;
+
+/**
+ * Author:  @just_for_happy 开心就好
+ * Date: 2016-03-01
+ * Time: 17:42
+ * FIXME  查看我的位置和  对方的位置
+ */
+public class LocationActivity  extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+
+
+    }
+}
